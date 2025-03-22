5da1efcbae708f91d900688639615d019dbc3574
Aphelion Web Development <17068516+Aphelion-im@users.noreply.github.com>
2025-03-22 23:03:54 +0100

    Delete Factorial directory

--- Factorial/Factorial/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// https://dotnettutorials.net/lesson/factorial-program-in-csharp/
-Console.WriteLine("Enter a number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int factorial = 1;
-for (int i = 1; i <= num; i++)
-{
-  factorial *= i;
-}
-Console.Write($"{factorial}");
