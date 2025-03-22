4c5956375c2546d3cfe10ac99bc6efa9b9d3ee2d
Aphelion-im <aphelion-im@protonmail.com>
2025-03-22 23:00:23 +0100

    Initial commit

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,9 @@
+﻿// https://dotnettutorials.net/lesson/factorial-program-in-csharp/
+Console.WriteLine("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int factorial = 1;
+for (int i = 1; i <= num; i++)
+{
+  factorial *= i;
+}
+Console.Write($"{factorial}");
